b4af037d60e6918a47213eeaeb5410b4fbd29dd6
onurr_k <145716470+onurrrk@users.noreply.github.com>
2025-07-11 19:10:08 +0300

    Update PlayerJoin.java

--- Mysql Inventory Bridge/src/net/craftersland/bridge/inventory/events/PlayerJoin.java
+++ Mysql Inventory Bridge/src/net/craftersland/bridge/inventory/events/PlayerJoin.java
@@ -2,39 +2,37 @@ package net.craftersland.bridge.inventory.events;
 
 import net.craftersland.bridge.inventory.Inv;
 import net.craftersland.bridge.inventory.objects.SyncCompleteTask;
-
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoin implements Listener {
-	
+
 	private Inv inv;
-	
+
 	public PlayerJoin(Inv inv) {
 		this.inv = inv;
 	}
-	
+
 	@EventHandler
-	public void onLogin(final PlayerJoinEvent event) {		
-		if (Inv.isDisabling == false) {
-			final Player p = event.getPlayer();
-			Bukkit.getScheduler().runTaskLaterAsynchronously(inv, new Runnable() {
-
-				@Override
-				public void run() {
-					if (p != null) {
-						if (p.isOnline() == true) {
-							inv.getInventoryDataHandler().onJoinFunction(p);
-							new SyncCompleteTask(inv, System.currentTimeMillis(), p).runTaskTimerAsynchronously(inv, 5L, 20L);
-						}
-					}
-				}
-				
-			}, 5L);
+	public void onLogin(final PlayerJoinEvent event) {
+		if (Inv.isDisabling) {
+			return;
 		}
-	}
 
+		final Player p = event.getPlayer();
+
+		p.getScheduler().runDelayed(inv, (scheduledTask) -> {
+			if (p.isOnline()) {
+				inv.getInventoryDataHandler().onJoinFunction(p);
+				
+				SyncCompleteTask syncTask = new SyncCompleteTask(inv, System.currentTimeMillis(), p);
+
+				p.getScheduler().runAtFixedRate(inv, (repeatingTask) -> {
+					syncTask.run(repeatingTask);
+				}, null, 5L, 20L);
+			}
+		}, null, 5L);
+	}
 }

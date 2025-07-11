0d609b45a5bf30271c54aac1b910426344c44b67
onurr_k <145716470+onurrrk@users.noreply.github.com>
2025-07-11 19:48:48 +0300

    Update InventorySyncTask.java

--- Mysql Inventory Bridge/src/net/craftersland/bridge/inventory/objects/InventorySyncTask.java
+++ Mysql Inventory Bridge/src/net/craftersland/bridge/inventory/objects/InventorySyncTask.java
@@ -1,57 +1,48 @@
 package net.craftersland.bridge.inventory.objects;
 
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
-
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import net.craftersland.bridge.inventory.Inv;
 
-public class InventorySyncTask extends BukkitRunnable {
-	
+public class SyncCompleteTask {
+
 	private Inv pd;
 	private long startTime;
 	private Player p;
 	private boolean inProgress = false;
-	private InventorySyncData syncD; 
-	
-	public InventorySyncTask(Inv pd, long start, Player player, InventorySyncData syncData) {
+
+	public SyncCompleteTask(Inv pd, long start, Player player) {
 		this.pd = pd;
 		this.startTime = start;
 		this.p = player;
-		this.syncD = syncData;
 	}
 
-	@Override
-	public void run() {
-		if (inProgress == false) {
-			if (p != null) {
-				if (p.isOnline() == true) {
-					inProgress = true;
-					DatabaseInventoryData data = pd.getInvMysqlInterface().getData(p);
-					if (data.getSyncStatus().matches("true")) {
-						pd.getInventoryDataHandler().setPlayerData(p, data, syncD, true);
-						inProgress = false;
-						this.cancel();
-					} else if (System.currentTimeMillis() - Long.parseLong(data.getLastSeen()) >= 600 * 1000) {
-						pd.getInventoryDataHandler().setPlayerData(p, data, syncD, true);
-						inProgress = false;
-						this.cancel();
-					} else if (System.currentTimeMillis() - startTime >= 22 * 1000) {
-						pd.getInventoryDataHandler().setPlayerData(p, data, syncD, true);
-						inProgress = false;
-						this.cancel();
-					}
-					inProgress = false;
-				} else {
-					//inProgress = false;
-					this.cancel();
-				}
-			} else {
-				//inProgress = false;
-				this.cancel();
+	public void run(ScheduledTask task) {
+		if (inProgress) {
+			return;
+		}
+
+		if (p == null || !p.isOnline()) {
+			task.cancel();
+			return;
+		}
+
+		inProgress = true;
+
+		if (pd.getInventoryDataHandler().isSyncComplete(p)) {
+			if (!pd.getConfigHandler().getString("ChatMessages.syncComplete").isEmpty()) {
+				p.sendMessage(pd.getConfigHandler().getStringWithColor("ChatMessages.syncComplete"));
+			}
+			pd.getSoundHandler().sendLevelUpSound(p);
+			task.cancel();
+		} else {
+			if (System.currentTimeMillis() - startTime >= 40 * 1000) {
+				task.cancel();
+			} else if (System.currentTimeMillis() - startTime >= 20 * 1000) {
+				pd.getInvMysqlInterface().setSyncStatus(p, "true");
 			}
 		}
-	}
-	
-	
 
+		inProgress = false;
+	}
 }

97b30d48bba86b234d24c9d6f284ac57d3a66c1d
onurr_k <145716470+onurrrk@users.noreply.github.com>
2025-07-11 19:27:36 +0300

    Update PlayerQuit.java

--- Mysql Inventory Bridge/src/net/craftersland/bridge/inventory/events/PlayerQuit.java
+++ Mysql Inventory Bridge/src/net/craftersland/bridge/inventory/events/PlayerQuit.java
@@ -10,30 +10,24 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerQuit implements Listener {
-	
-	private Inv inv;
-	
-	public PlayerQuit(Inv inv) {
-		this.inv = inv;
-	}
-	
-	@EventHandler
-	public void onDisconnect(final PlayerQuitEvent event) {
-		if (Inv.isDisabling == false) {
-			Bukkit.getScheduler().runTaskLaterAsynchronously(inv, new Runnable() {
 
-				@Override
-				public void run() {
-					if (event.getPlayer() != null) {
-						Player p = event.getPlayer();
-						ItemStack[] inventory = inv.getInventoryDataHandler().getInventory(p);
-						ItemStack[] armor = inv.getInventoryDataHandler().getArmor(p);
-						inv.getInventoryDataHandler().onDataSaveFunction(p, true, "true", inventory, armor);
-					}
-				}
-				
-			}, 2L);
-		}
-	}
+    private final Inv inv;
 
+    public PlayerQuit(Inv inv) {
+        this.inv = inv;
+    }
+
+    @EventHandler
+    public void onDisconnect(final PlayerQuitEvent event) {
+        if (!Inv.isDisabling) {
+            Player p = event.getPlayer();
+            if (p != null) {
+                Bukkit.getRegionScheduler().runDelayed(inv, p.getLocation(), task -> {
+                    ItemStack[] inventory = inv.getInventoryDataHandler().getInventory(p);
+                    ItemStack[] armor = inv.getInventoryDataHandler().getArmor(p);
+                    inv.getInventoryDataHandler().onDataSaveFunction(p, true, "true", inventory, armor);
+                }, 2L);
+            }
+        }
+    }
 }
